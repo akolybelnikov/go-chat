19f03a738f8d3cd654bfdb7e55ef8334bfe505f0
akolybelnikov <a.kolybelnikov@gmail.com>
2018-10-01 21:41:12 +0200

    handle messages and users concurrently

--- room.go
+++ room.go
@@ -1,7 +1,7 @@
 package main
 
 type room struct {
-	// forward is a channel that holds incoming messages 
+	// forward is a channel that holds incoming messages
 	// that should be forwarded to other clients
 	forward chan []byte
 
@@ -13,4 +13,23 @@ type room struct {
 
 	// clients holds all current clients in this room.
 	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
 }
